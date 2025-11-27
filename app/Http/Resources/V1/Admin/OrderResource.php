65a87db5e7d15784dc1db64243c1e4bc9ad0756f
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-27 22:15:54 +0700

    fixing riwayat pembayaran

--- app/Http/Resources/V1/Admin/OrderResource.php
+++ app/Http/Resources/V1/Admin/OrderResource.php
@@ -33,7 +33,34 @@ class OrderResource extends JsonResource
             'billing_address' => $this->billingAddress?->full_address,
             'order_items' => OrderItemResource::collection($this->whenLoaded('items')),
             'invitation_detail' => new InvitationDetailResource($this->whenLoaded('invitationDetail')),
-            'payments' => $this->payments,
+            'payments' => $this->whenLoaded('payments', function () {
+                return $this->payments->map(function ($payment) {
+                    $method = $payment->payment_gateway;
+                    
+                    if (!empty($payment->raw_response) && isset($payment->raw_response['payment_type'])) {
+                        $type = $payment->raw_response['payment_type'];
+                        $method = ucwords(str_replace('_', ' ', $type));
+                        
+                        if ($type === 'bank_transfer') {
+                            if (isset($payment->raw_response['va_numbers'][0]['bank'])) {
+                                $method .= ' - ' . strtoupper($payment->raw_response['va_numbers'][0]['bank']);
+                            } elseif (isset($payment->raw_response['permata_va_number'])) {
+                                $method .= ' - PERMATA';
+                            }
+                        } elseif ($type === 'cstore' && isset($payment->raw_response['store'])) {
+                            $method .= ' - ' . ucfirst($payment->raw_response['store']);
+                        }
+                    }
+                    
+                    return [
+                        'id' => $payment->id,
+                        'payment_date' => $payment->created_at,
+                        'amount' => $payment->amount,
+                        'payment_method' => $method,
+                        'status' => $payment->status,
+                    ];
+                });
+            }),
         ];
     }
 }
